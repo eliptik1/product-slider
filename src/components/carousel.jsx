ea9e2699a47efa3b40a15f6a9adfd86c9f07b8b5
eliptik <adamfeyk5@gmail.com>
2023-12-10 05:56:26 +0300

    Add color switch feature

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -5,6 +5,7 @@ export const Carousel = () => {
   const [isDragging, setIsDragging] = useState(false);
   const [startX, setStartX] = useState(0);
   const [startScrollLeft, setStartScrollLeft] = useState(0);
+  const [selectedColors, setSelectedColors] = useState({});
 
   const carouselRef = useRef(null);
 
@@ -29,6 +30,13 @@ export const Carousel = () => {
     carouselRef.current.scrollLeft = startScrollLeft - (e.pageX - startX);
   };
 
+  const handleColorChange = (itemId, color) => {
+    setSelectedColors((prevSelectedColors) => ({
+      ...prevSelectedColors,
+      [itemId]: color,
+    }));
+  };
+
   useEffect(() => {
     const carousel = carouselRef.current;
 
@@ -44,37 +52,85 @@ export const Carousel = () => {
   }, [isDragging, startX, startScrollLeft]);
 
   let items = [
-    {
-        description: "test-description",
-        image: require("../Assets/Images/EG009/EG009-Y.jpg")
+    {   
+        id: 1,
+        description: "Engagement Ring 1",
+        price: "$101.00 USD",
+        image: {
+            yellow: require('../Assets/Images/EG009/EG009-Y.jpg'),
+            white: require('../Assets/Images/EG009/EG009-W.jpg'),
+            rose: require('../Assets/Images/EG009/EG009-R.jpg')
+        }
     },
     {
-        description: "test-description",
-        image: require("../Assets/Images/EG009/EG009-W.jpg")
+        id: 2,
+        description: "Engagement Ring 2",
+        price: "$101.00 USD",
+        image: {
+            yellow: require("../Assets/Images/EG012/EG012-Y.jpg"),
+            white: require("../Assets/Images/EG012/EG012-W.jpg"),
+            rose: require("../Assets/Images/EG012/EG012-R.jpg")
+        }
     },
     {
-        description: "test-description",
-        image: require("../Assets/Images/EG009/EG009-W.jpg")
+        id: 3,
+        description: "Engagement Ring 3",
+        price: "$101.00 USD",
+        image: {
+            yellow: require("../Assets/Images/EG020/EG020-200P-Y.jpg"),
+            white: require("../Assets/Images/EG020/EG020-200P-W.jpg"),
+            rose: require("../Assets/Images/EG020/EG020-200P-R.jpg")
+        }
     },
     {
-        description: "test-description",
-        image: require("../Assets/Images/EG009/EG009-W.jpg")
+        id: 4,
+        description: "Engagement Ring 4",
+        price: "$101.00 USD",
+        image: {
+            yellow: require("../Assets/Images/EG022/EG022-100P-Y.jpg"),
+            white: require("../Assets/Images/EG022/EG022-100P-W.jpg"),
+            rose: require("../Assets/Images/EG022/EG022-100P-R.jpg")
+        }
     },
     {
-        description: "test-description",
-        image: require("../Assets/Images/EG009/EG009-W.jpg")
+        id: 5,
+        description: "Engagement Ring 5",
+        price: "$101.00 USD",
+        image: {
+            yellow: require("../Assets/Images/EG074/EG074-200P-Y.jpg"),
+            white: require("../Assets/Images/EG074/EG074-100P-W.jpg"),
+            rose: require("../Assets/Images/EG074/EG074-200P-R.jpg")
+        }
     },
     {
-        description: "test-description",
-        image: require("../Assets/Images/EG009/EG009-W.jpg")
+        id: 6,
+        description: "Engagement Ring 6",
+        price: "$101.00 USD",
+        image: {
+            yellow: require("../Assets/Images/EG075/EG075-200P-Y.jpg"),
+            white: require("../Assets/Images/EG075/EG075-200P-W.jpg"),
+            rose: require("../Assets/Images/EG075/EG075-200P-R.jpg")
+        }
     },
     {
-        description: "test-description",
-        image: require("../Assets/Images/EG009/EG009-W.jpg")
+        id: 7,
+        description: "Engagement Ring 7",
+        price: "$101.00 USD",
+        image: {
+            yellow: require("../Assets/Images/EG094/EG094-200P-Y.jpg"),
+            white: require("../Assets/Images/EG094/EG094-200P-W.jpg"),
+            rose: require("../Assets/Images/EG094/EG094-200P-R.jpg")
+        }
     },
     {
-        description: "test-description",
-        image: require("../Assets/Images/EG009/EG009-R.jpg")
+        id: 8,
+        description: "Engagement Ring 8",
+        price: "$101.00 USD",
+        image: {
+            yellow: require("../Assets/Images/EG115/EG115-100P-Y.jpg"),
+            white: require("../Assets/Images/EG115/EG115-100P-W.jpg"),
+            rose: require("../Assets/Images/EG115/EG115-100P-R.jpg")
+        }
     }
 ]
 
@@ -98,7 +154,11 @@ export const Carousel = () => {
         className="carousel scroll-smooth snap-x snap-mandatory w-full overflow-x-scroll grid grid-flow-col gap-3 "
       >
         {items.map((item) => (
-          <CarouselItem item={item} />
+          <CarouselItem 
+            item={item} 
+            selectedColor={selectedColors[item.id] || 'yellow'}
+            onColorChange={(color) => handleColorChange(item.id, color)} 
+          />
         ))}
       </ul>
 
--- src/components/carouselItem.jsx
+++ src/components/carouselItem.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 
-export const CarouselItem = ({item}) => {
+export const CarouselItem = ({item, selectedColor, onColorChange}) => {
     return (
-    <li className="card snap-start h-[340px] flex flex-col justify-center items-center w-full">
+    <li className="card snap-start flex flex-col justify-center items-center w-full  ">
         <div>
-            <img className="w-[250px] h-[250px] object-cover select-none" src={item.image} alt="image" draggable="false" />
+            <img className="w-[250px] h-[250px] object-cover select-none rounded-xl mt-14" src={item.image[selectedColor]} alt="image" draggable="false" />
             <div>
                 <h2 className="mt-2">{item.description}</h2>
-                <span>Lorem, ipsum dolor.</span>
+                <span>{item.price}</span>
             </div>
+
+            <div className="color-buttons flex gap-4">
+            {Object.keys(item.image).map((color, index) => (
+            <button
+                key={index}
+                onClick={() => onColorChange(color)}
+                className={
+                    `w-5 h-5 rounded-[50%] mt-5
+                    bg-[${index === 0 ? "#E6CA97" : index === 1 ? "#D9D9D9" : "#E1A4A9" }] 
+                    ${color === selectedColor ? 'circle-border' : ''}`}
+                >
+            </button>
+        ))}
+        </div>
+        <div className="mb-10 mt-2">
+            {selectedColor == "yellow" ? "Yellow Gold" : selectedColor == "white" ? "White Gold" : "Rose Gold" }
+        </div>
         </div>
     </li>
     )
